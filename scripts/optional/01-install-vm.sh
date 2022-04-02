f1b7bd3423d4665601a4ddb6cb4b3e63a0acade0
Niklas Feurstein <nik.feurstein@gmail.com>
2022-04-02 19:44:48 +0200

    remove the optional install script of kvm.

--- scripts/optional/01-install-vm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Sy --needed virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat
-
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
-
-usermod -a -G libvirt "$USER"
