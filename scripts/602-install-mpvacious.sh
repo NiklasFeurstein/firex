61c61de208e5eec9bd9a9690582cdfa05891bd5c
Niklas Feurstein <nik.feurstein@gmail.com>
2022-09-28 17:34:10 +0200

    Fix directory bug mpvacious

--- scripts/602-install-mpvacious.sh
+++ scripts/602-install-mpvacious.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Installing/Updating mpvacious now"
 
+logfilepath=$(pwd)
 git clone 'https://github.com/Ajatt-Tools/mpvacious.git' ~/.config/mpv/scripts/subs2srs > /dev/null 2>> log.txt
-cd ~/.config/mpv/scripts/subs2srs 2>> "$(pwd)/log.txt" && git pull > /dev/null 2>> "$(pwd)/log.txt"
+cd ~/.config/mpv/scripts/subs2srs 2>> "$logfilepath/log.txt" && git pull > /dev/null 2>> "$logfilepath/log.txt"
