2a033d7262ac4bae08cbda80893e6ab18a217be5
Niklas Feurstein <nik.feurstein@gmail.com>
2022-11-16 23:07:26 +0100

    Added bookmark to Pictures folder. Adjusted mpv screenshot names and changed some hotkeys.

--- scripts/700-adjust-bookmarks.sh
+++ scripts/700-adjust-bookmarks.sh
@@ -2,11 +2,15 @@
 
 FILE="./personal/desktop-configs/1/.config/gtk-3.0/bookmarks"
 [ -f "$FILE" ] || { echo "ERROR finding $FILE."; exit 1; }
-STRING="file:///home/$USER/Downloads"
-if grep -q -F -x "$STRING" "$FILE";
-then
-  echo "Already Created Bookmark";
-else
-  echo "$STRING" >> $FILE
-  echo "Created Boomark $STRING";
-fi
+
+PREFIX="file:///home/$USER/"
+STRINGS=("Downloads" "Pictures")
+for STRING in ${STRINGS[@]}; do
+  if grep -qFx "${PREFIX}${STRING}" "$FILE";
+  then
+    echo "Already Created Bookmark";
+  else
+    echo "${PREFIX}${STRING}" >> $FILE
+    echo "Created Boomark ${PREFIX}${STRING}";
+  fi
+done
