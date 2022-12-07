b364f56aacef2ec27a0d8433cb5d2423ae93c070
Niklas Feurstein <nik.feurstein@gmail.com>
2022-12-08 00:03:46 +0100

    Added shell script to automatically rename subtitle files

---
+++ personal/settings/.bin/subtitleFileRename.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+###############################################################
+# Renames the subtitle files in this or a direct subdirectory #
+#      starting with "sub" to the name of the video file      #
+###############################################################
+
+shopt -s nullglob dotglob
+foundVideoFiles=({*.mp4,*.mkv})
+numberOfVideoFiles=${#foundVideoFiles[@]}
+
+if [[ numberOfVideoFiles -eq 0 ]]
+then
+    echo "ERROR: No video files found!" 1>&2
+    exit 128
+fi
+
+foundSubtitleFiles=({*.ass,*.srt,sub**/*.ass,sub**/*.srt})    # Same directory and direct subdirectory starting with "sub"
+numberOfSubtitleFiles=${#foundSubtitleFiles[@]}
+if [[ numberOfSubtitleFiles -eq 0 ]]
+then
+    echo "ERROR: No subtitle files found!" 1>&2
+    exit 129
+fi
+
+if [[ numberOfVideoFiles -ne numberOfSubtitleFiles ]]
+then
+
+    echo $"----Number of video files (${numberOfVideoFiles}) and subtitle files (${numberOfSubtitleFiles}) differ!----"
+    read -rep $'--------Continue regardless? [Yy]--------\n' -n 1
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "ERROR: Number of video and subtitle files differ!" 1>&2
+        exit 130
+    fi
+fi
+
+for counter in $(seq 0 $[numberOfVideoFiles-1])
+do
+    fullNameOfVideo="$(basename "${foundVideoFiles[$counter]}")"
+    fileNameOfVideo="${fullNameOfVideo%.*}"
+    
+    directoryOfSubs="$(dirname "${foundSubtitleFiles[$counter]}")"
+    fullNameOfSubtitle="$(basename "${foundSubtitleFiles[$counter]}")"
+    subtitleFileExtension="${fullNameOfSubtitle##*.}"
+
+    newNameOfSubtitle="${directoryOfSubs}/${fileNameOfVideo}.ja.${subtitleFileExtension}"
+
+    mv "${foundSubtitleFiles[$counter]}" "${newNameOfSubtitle}"
+done
+
+echo "Successfully renamed ${numberOfVideoFiles} subtitle files."
